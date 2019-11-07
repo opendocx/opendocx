fb8fdf2ac0ae9790ae825a0b5226bfc5c0c2f0d9
Lowell Stewart <lowell@opendocx.com>
2019-11-07 14:55:52 -0700

    yatte update for new scope handling

--- src/docx-evaluator.js
+++ src/docx-evaluator.js
@@ -1,115 +1,106 @@
 'use strict';
 
-const { ContextStack, Engine } = require('yatte');
+const { Scope, Engine } = require('yatte');
 const XmlDataBuilder = require('./xmlbuilder')
 const version = require('./version');
 const semver = require('semver')
 
 class XmlAssembler {
-    constructor (context, locals = null) {
-        this.context = context;
-        this.locals = locals;
-        this.missing = {};
-        this.contextStack = new ContextStack();
-        this.xmlStack = new XmlDataBuilder();
+  constructor (context, locals = null) {
+    this.missing = {};
+    this.contextStack = null;
+    if (context) {
+      this.contextStack = Scope.pushObject(context, this.contextStack);
     }
+    if (locals) {
+      this.contextStack = Scope.pushObject(locals, this.contextStack);
+    }
+    this.xmlStack = new XmlDataBuilder();
+  }
 
-    loadTemplateModule(templateJsFile) {
-        const thisVers = semver.major(version) + '.' + semver.minor(version)
-        const extractedLogic = require(templateJsFile);
-        const loadedVers = extractedLogic.version
-        if (loadedVers && semver.eq(version, loadedVers) || semver.satisfies(loadedVers, thisVers)) {
-            return extractedLogic
-        } // else
-        // invalidate loaded module with incorrect version!
-        delete require.cache[require.resolve(templateJsFile)]
-        throw new Error(`Version mismatch: Expecting template JavaScript version ${thisVers}.x, but JS file is version ${loadedVers}`)
-    }
+  loadTemplateModule(templateJsFile) {
+    const thisVers = semver.major(version) + '.' + semver.minor(version)
+    const extractedLogic = require(templateJsFile);
+    const loadedVers = extractedLogic.version
+    if (loadedVers && semver.eq(version, loadedVers) || semver.satisfies(loadedVers, thisVers)) {
+      return extractedLogic
+    } // else
+    // invalidate loaded module with incorrect version!
+    delete require.cache[require.resolve(templateJsFile)]
+    throw new Error(`Version mismatch: Expecting template JavaScript version ${thisVers}.x, but JS file is version ${loadedVers}`)
+  }
 
-    assembleXml(templateJsFile, joinstr = "") {
-        const extractedLogic = this.loadTemplateModule(templateJsFile);
-        extractedLogic.evaluate(this.context, this.locals, this);
-        return this.xmlStack.toString(joinstr);
-    }
+  assembleXml(templateJsFile, joinstr = "") {
+    const extractedLogic = this.loadTemplateModule(templateJsFile);
+    extractedLogic.evaluate(this.contextStack, null, this);
+    return this.xmlStack.toString(joinstr);
+  }
 
-    beginObject(ident, objContext, objLocals) {
-        if (this.contextStack.empty()) {
-            this.contextStack.pushGlobal(objContext, objLocals);
-        } else {
-            this.contextStack.pushObject(ident, objContext);
-            this.xmlStack.pushObject(ident); //(this.contextStack.peekName())
-        }
+  beginObject(ident, objContext, objLocals) {
+    if (objContext !== this.contextStack && typeof objContext === 'number') {
+      this.contextStack = Scope.pushListItem(objContext, this.contextStack);
+      this.xmlStack.pushObject(ident);
     }
-    
-    endObject() {
-        const frame = this.contextStack.popObject();
-        this.xmlStack.popObject()
+  }
+  
+  endObject() {
+    this.contextStack = Scope.pop(this.contextStack);
+    this.xmlStack.popObject()
+  }
+  
+  define(ident, expr) {
+    if (Scope.empty(this.contextStack)) {
+      throw new Error('internal error: Cannot define a member on an empty context stack');
     }
-    
-    define(ident, expr) {
-        if (this.contextStack.empty()) {
-            throw new Error('internal error: Cannot define a member on an empty context stack');
-        }
-        const frame = this.contextStack.peek();
-        if (frame.type != 'Object') {
-            throw new Error(`Internal error: cannot define a member on a ${frame.type} context`);
-        }
-    
-        const evaluator = Engine.compileExpr(expr); // these are cached so this should be fast
-        let value = frame.evaluate(evaluator); // we need to make sure this is memoized to avoid unnecessary re-evaluation
-        if (value && (typeof value === 'object') && (value.errors || value.missing)) { // value is a yatte EvaluationResult, probably because of nested template evaluation
-            value = value.valueOf() // disregard everything but the actual evaluated value
-        }
-        if (value === null || typeof value === 'undefined') {
-            this.missing[expr] = true;
-            value = '[' + expr + ']'; // missing value placeholder
-        }
-        if (value === '') {
-            this.xmlStack.set(ident, undefined);
-        } else {
-            this.xmlStack.set(ident, value);
-        }
+    const frame = this.contextStack;
+    if (frame._scopeType != Scope.OBJECT) {
+      throw new Error(`Internal error: cannot define a member on a ${frame._scopeType} context`);
     }
-    
-    beginCondition(ident, expr) {
-        if (this.contextStack.empty()) {
-            throw new Error('internal error: Cannot define a condition on an empty context stack');
-        }
-        const frame = this.contextStack.peek();
-        if (frame.type != 'Object') {
-            throw new Error(`Internal error: cannot define a condition on a ${frame.type} context`);
-        }
-        const evaluator = Engine.compileExpr(expr); // these are cached so this should be fast
-        const value = frame.evaluate(evaluator); // we need to make sure this is memoized to avoid unnecessary re-evaluation
-        const bValue = ContextStack.IsTruthy(value);
-        this.xmlStack.set(ident, bValue);
-        return bValue;
+  
+    const evaluator = Engine.compileExpr(expr); // these are cached so this should be fast
+    let value = frame._evaluate(evaluator); // we need to make sure this is memoized to avoid unnecessary re-evaluation
+    if (value && (typeof value === 'object') && (value.errors || value.missing)) { // value is a yatte EvaluationResult, probably because of nested template evaluation
+      value = value.valueOf() // disregard everything but the actual evaluated value
     }
-    
-    beginList(ident, expr) {
-        const frame = this.contextStack.peek();
-        const evaluator = Engine.compileExpr(expr); // these are cached so this should be fast
-        let iterable = frame.evaluate(evaluator); // we need to make sure this is memoized to avoid unnecessary re-evaluation
-        const indices = this.contextStack.pushList(ident, iterable);
-        this.xmlStack.pushList(ident)
-        return indices;
+    if (value === null || typeof value === 'undefined') {
+      this.missing[expr] = true;
+      value = '[' + expr + ']'; // missing value placeholder
     }
-    
-    endList() {
-        this.xmlStack.popList()
-        this.contextStack.popList();
+    if (value === '') {
+      this.xmlStack.set(ident, undefined);
+    } else {
+      this.xmlStack.set(ident, value);
     }
+  }
+  
+  beginCondition(ident, expr) {
+    if (Scope.empty(this.contextStack)) {
+      throw new Error('internal error: Cannot define a condition on an empty context stack');
+    }
+    const frame = this.contextStack;
+    if (frame._scopeType != Scope.OBJECT) {
+      throw new Error(`Internal error: cannot define a condition on a ${frame._scopeType} context`);
+    }
+    const evaluator = Engine.compileExpr(expr); // these are cached so this should be fast
+    const value = frame._evaluate(evaluator); // we need to make sure this is memoized to avoid unnecessary re-evaluation
+    const bValue = Scope.isTruthy(value);
+    this.xmlStack.set(ident, bValue);
+    return bValue;
+  }
+  
+  beginList(ident, expr) {
+    const frame = this.contextStack;
+    const evaluator = Engine.compileExpr(expr); // these are cached so this should be fast
+    let iterable = frame._evaluate(evaluator); // we need to make sure this is memoized to avoid unnecessary re-evaluation
+    this.contextStack = Scope.pushList(iterable, this.contextStack, ident);
+    const indices = this.contextStack._indices;
+    this.xmlStack.pushList(ident)
+    return indices;
+  }
+  
+  endList() {
+    this.xmlStack.popList()
+    this.contextStack = Scope.pop(this.contextStack)
+  }
 }
 module.exports = XmlAssembler;
-
-const escapeXml = function (str) {
-    return str.replace(/[<>&'"]/g, function (c) {
-        switch (c) {
-            case '<': return '&lt;';
-            case '>': return '&gt;';
-            case '&': return '&amp;';
-            case '\'': return '&apos;';
-            case '"': return '&quot;';
-        }
-    });
-}
